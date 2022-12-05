7119791ad18e1fc07bbe67c78c05dd6e8710ca87
angel <angeeliaw@gmail.com>
2022-12-05 18:24:18 +0700

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,13 +4,15 @@ const jwt = require('jsonwebtoken');
 SECRET = process.env.SECRET
 const Auth = {
     verifyToken(req, res, next){
-        token= req.cookies["JWT"]
+        // token= req.cookies["JWT"]
         // console.log(token);
+        const {token} = req.body
         if (token) {
             // 12. Lalukan jwt verify 
             const verified = jwt.verify(token,SECRET)
             if(verified){
               console.log("Verification succeedd")
+              req.verified = verified
               return next();
             }else{
               return res.status(401).send(error);
